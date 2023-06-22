799fd73d5eafaa5332bd5a6674c01a6095b0fde5
Muhammed Sanid <shanid774@gmail.com>
2023-06-22 21:01:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React ,{useState}from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import "./App.css";
+
+
+const App = () => {
+  const [input , setInput]=useState('')
+  const [ todos,setTodos]=useState([])
+  const [editTodo,setEditTodo]=useState(null)
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          editTodo={editTodo} 
+          setEditTodo={setEditTodo}/>
+        </div>
+        <div >
+          <TodoList todos={todos} setTodos={setTodos}  setEditTodo={setEditTodo}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "../App.css";
+
+const TodoList = ({ todos, setTodos, setEditTodo }) => {
+  const handleDelete = ({ id }) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleComplete = (todo, completed) => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
+  };
+  const handleEdit = ({ id }) => {
+    const findTodo = todos.find((todo) => todo.id === id);
+    setEditTodo(findTodo);
+  };
+  return (
+    <div>
+      {todos.map((todo) => (
+        <li className="list-item" key={todo.id}>
+          <input
+            type="text"
+            value={todo.title}
+            className="list"
+            onChange={(event) => event.preventDefault()}
+          />
+          <div>
+            <button
+              className="button-complete task-button"
+              onClick={() => handleComplete(todo)}
+              style={{ color: todo.completed ? "green" : "white" }}
+            >
+              <i className="fa fa-check-circle"></i>
+            </button>
+            <button
+              className="button-edit task-button"
+              onClick={() => handleEdit(todo)}
+            >
+              <i className="fa fa-edit"></i>
+            </button>
+            <button
+              className="button-delete task-button"
+              onClick={() => handleDelete(todo)}
+            >
+              <i className="fa fa-trash"></i>
+            </button>
+          </div>
+        </li>
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
